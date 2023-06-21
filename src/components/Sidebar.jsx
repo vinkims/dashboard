fd6e5e11046b3f17f3a1e8c1ec2b603ee3efb752
vinkims <vincekigen@gmail.com>
2023-06-21 16:56:46 +0300

    feat(Components)- Add Header, SparkLine and Stacked components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -37,7 +37,7 @@ const Sidebar = () => {
             </button>
           </TooltipComponent>
         </div>
-        <div classname="mt-10">
+        <div className="mt-10">
           {links.map((item) => (
             <div key={item.title}>
               <p className="text-gray-400 m-3 mt-4 uppercase">

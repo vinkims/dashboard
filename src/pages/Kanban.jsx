9fc1d93f1df8b6e1ff488604bba5b0dfe58cdcc9
vinkims <vincekigen@gmail.com>
2023-06-22 12:38:56 +0300

    feat(Components)- Add missing pages

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -8,11 +8,14 @@ const Kanban = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Kanban" />
-      <KanbanComponent>
+      <KanbanComponent
+        id="kanbn"
+        dataSource={kanbanData}
+        cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
+        keyField="Status"
+      >
         <ColumnsDirective>
-          {kanbanGrid.map((item, index) => (
-            <ColumnDirective key={index} {...item} />
-          ))}
+          {kanbanGrid.map((item, index) => <ColumnDirective key={index} {...item} /> )}
         </ColumnsDirective>
       </KanbanComponent>
     </div>
